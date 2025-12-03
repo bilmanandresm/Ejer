79b656997c2d6d1d245f469c58293c3cace01e52
bilmanandresm <bilmanandresm@gmail.com>
2025-12-03 09:36:39 -0500

    Primer ejercicio

---
+++ ejercicio.py
@@ -0,0 +1,9 @@
+
+contrasena = "admin123"
+
+escribe = input("Ingresa la contraseña: ")
+
+if contrasena == escribe:
+    print("Contrseña Correcta Felicidades !!!")
+else:
+    print("Contraseña incorrecta. por favor Inetentelo de nuevo ")
